7018225546dfaab82baaf6a60533f5771925e027
100mango <100mango@gmail.com>
2013-10-26 09:42:10 +0800

    a

--- WereWolf/PXFunctionViewController.m
+++ WereWolf/PXFunctionViewController.m
@@ -67,7 +67,7 @@
         NSString *picName = [NSString stringWithFormat:@"small%d",[self.manager getRoleTypeWithTag:i]];
         button.tag = i;
         button.image.image = [UIImage imageNamed:picName];
-        [button setBackgroundImage:[UIImage imageNamed:@"cardBack"] forState:UIControlStateNormal];
+        [button setBackgroundImage:[UIImage imageNamed:@"cardBackName"] forState:UIControlStateNormal];
         [self.scrollView addSubview:button];
         [self.buttonArray addObject:button];
     }
